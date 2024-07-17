dbe165b09c2e4fbb9603696e9624849e9097d976
jovimoura <joaovictors.mouraa@gmail.com>
2024-07-17 00:33:39 -0300

    change icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,14 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-import { CloudRainWind } from "lucide-react";
-
 import { buttonVariants } from "@/components/ui/button";
-import { IconGitHub } from "@/components/ui/icons";
+import { IconGitHub, IconOpenAI } from "@/components/ui/icons";
 
 export function Header() {
   return (
     <header className='sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl'>
       <div className='flex items-center'>
-        <CloudRainWind className="text-primary" />
+        <IconOpenAI />
       </div>
 
       <span className="font-bold text-primary">Chatbot Next.js</span>
